dd4b113a8fd624d27083942c2e0289d4e34d1754
broodjesemih <justlikesemih04@gmail.com>
2025-09-04 11:46:00 +0200

    Add initial implementation of les_pkg with template_general node
    - Created CMakeLists.txt and package.xml for the les_pkg package.
    - Implemented a basic ROS2 node (template_general) that logs a greeting message.
    - Configured dependencies including rclcpp and ament_cmake.
    - Set up build and install configurations for the package.
    - Added logging functionality to the node to confirm successful initialization.

---
+++ build/les_pkg/CMakeFiles/template_general.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/template_general.dir/src/template_general.cpp.o"
+  "CMakeFiles/template_general.dir/src/template_general.cpp.o.d"
+  "template_general"
+  "template_general.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/template_general.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
